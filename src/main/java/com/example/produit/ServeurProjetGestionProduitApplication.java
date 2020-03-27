8d9e15430b8774210e4d483b0c1cb9a414f31a11
Romuald Mengue <romuald.mengue@cam.alpiwood.com>
2020-03-27 17:17:24 +0100

    premier commit

---
+++ src/main/java/com/example/produit/ServeurProjetGestionProduitApplication.java
@@ -0,0 +1,13 @@
+package com.example.produit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurProjetGestionProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurProjetGestionProduitApplication.class, args);
+	}
+
+}
